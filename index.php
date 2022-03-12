15b892c9c702e1a69661056c85e5294efa53d6bd
fatih-tutar <fatihtutar93@gmail.com>
2022-03-12 14:19:18 +0300

    Reading Json Datas

--- index.php
+++ index.php
@@ -19,4 +19,51 @@
     foreach($object as $key => $value){
         echo $key." : ".$value."<br/>";
     }
+
+    // Extracting all datas (keys and values) from json file
+    function space(){
+        return "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    }
+    echo "<br/>-----Extracting All Datas (keys and values) From Json File-----<br/>";
+    $datas = json_decode(file_get_contents("object.json"));
+    foreach($datas as $key => $value){
+        if(is_string($value)){
+            echo $key." : ".$value."<br/>";
+        }elseif (is_numeric($value)) {
+            echo $key . " : " . $value . "<br/>";
+        }elseif(is_array($value)){
+            $a_variable = $value;
+            foreach ($a_variable as $a_key => $a_value) {
+                if($a_key == 0){
+                    echo $key . " :<br/>". space() . $a_value . "<br/>";
+                }else{
+                    echo space() . $a_value . "<br/>";
+                }              
+            }
+        }elseif(is_object($value)){
+            $o_variable = $value;
+            $i = 0;
+            foreach ($o_variable as $o_key => $o_value) {
+                if($i == 0){
+                    echo $key . " :<br/>" .space().$o_key." : ". $o_value . "<br/>";
+                }else{
+                    echo space() .$o_key." : ". $o_value . "<br/>";
+                }      
+                $i++;          
+            }
+        }
+    }
+    echo "<br/># We can do this with shorter code by converting everything to array<br/>";
+    echo "<br/>-----Extracting All Datas (keys and values) WITH ARRAY CONVERTING From Json File -----<br/>";
+    $arrayDatas = json_decode(file_get_contents("object.json"),true);
+    function readJson($text){
+        foreach ($text as $key => $value) {
+            if(!is_array($value)){
+                echo $key." : ".$value."<br/>";
+            }else{
+                readJson($value);
+            }
+        }
+    }
+    readJson($arrayDatas);
 ?>
